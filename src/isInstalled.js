eec85765578d556fa26b85e948a8db7f8f33c263
vega-wong <vega.wong.work@gmail.com>
2017-01-19 17:28:46 +0800

    up

--- src/isInstalled.js
+++ src/isInstalled.js
@@ -1,8 +1,5 @@
 const path = require('path');
 module.exports = function (name) {
-  if (name === 'babel-loader') {
-    console.log(require.resolve(name));
-  }
   try {
     require.resolve(name);
     return true;
