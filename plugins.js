c28d3c1ffc88bfa783bb771648c3abdf18104aa5
vega-wong <vega.wong.work@gmail.com>
2017-01-20 10:13:04 +0800

    up

--- plugins.js
+++ plugins.js
@@ -1,9 +1,9 @@
-const BuildHtmlWebpackPlugin = require('build-html-webpack-plugin');
+const BuildHtmlPlugin = require('build-html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
-  BuildHtmlWebpackPlugin,
+  BuildHtmlPlugin,
   ExtractTextPlugin,
   OpenBrowserPlugin
 };
